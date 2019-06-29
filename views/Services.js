aba8e381e98ce2a6fa796d1952c2b116de039933
amit223 <amit.moshe100@gmail.com>
2019-06-29 19:49:28 +0300

    done register page

--- views/Services.js
+++ views/Services.js
@@ -1,35 +1,53 @@
-angular.module('myApp').service('regService',function($http) {
-    let self=this;
-    self.register= function(obj){
-        var req={
-            method:'POST',
-            url:'http://localhost:3000/create/'+JSON.stringify(obj),
+angular.module('myApp').service('regService', function ($http) {
+    let self = this;
+    self.register = function (obj) {
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:3000/create/' + JSON.stringify(obj),
 
-            headers:{
-                "Access-Control-Allow-Origin":"*",
-                "Access-Control-Allow-Methods":"GET, POST, PUT, DELETE, OPTIONS",
-                "Access-Control-Allow-Headers":"*",
-                "Access-Control-Max-Age":"*",
-                "Content-Type":"application/json"
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Max-Age": "*",
+                "Content-Type": "application/json"
             },
-            data:JSON.stringify(obj)
+            data: JSON.stringify(obj)
         };
         return $http(req);
     };
 
-    self.getPointOfInterests=function(){
-        var req={
-            method:'GET',
-            url:'http://localhost:3000/getPointOfInterests',
-            headers:{
-                "Access-Control-Allow-Origin":"*",
-                "Access-Control-Allow-Methods":"GET, POST, PUT, DELETE, OPTIONS",
-                "Access-Control-Allow-Headers":"*",
-                "Access-Control-Max-Age":"*",
-                "Content-Type":"application/json"
+    self.addCategoryToUser = function (obj) {
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:3000/addCategoryToUser/' + JSON.stringify(obj),
+
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Max-Age": "*",
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify(obj)
+        };
+        return $http(req);
+    };
+
+    self.getPointOfInterestsCategories = function () {
+        var req = {
+            method: 'GET',
+            url: 'http://localhost:3000/getPointOfInterestsCategories',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Max-Age": "*",
+                "Content-Type": "application/json"
             }
         };
         return $http(req);
     };
+
 });
 
